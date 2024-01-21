280f384a9fe9728af4a21d4130091301077dde43
absolutetsonga <abstsonga@gmail.com>
2024-01-21 16:59:24 +0600

    modified: custom user model username field

--- users/admin.py
+++ users/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin
 
-# from .forms import CustomUserCreationForm, CustomUserChangeForm
-# from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# class CustomUserAdmin(UserAdmin):
-#     add_form = CustomUserCreationForm
-#     form = CustomUserChangeForm
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
 
-#     model = CustomUser
+    model = CustomUser
 
-#     list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined')
-#     list_filter = ('is_active', 'is_superuser', 'is_staff')
+    list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined')
+    list_filter = ('is_active', 'is_superuser', 'is_staff')
     
-#     fieldsets = (None, {'fields': ('username', 'email', 'password', 'first_name', 'last_name')}), 
-#     ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-#     ('Dates', {'fields': ('last_login', 'date_joined')})
+    fieldsets = (None, {'fields': ('username', 'email', 'password', 'first_name', 'last_name')}), 
+    ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    ('Dates', {'fields': ('last_login', 'date_joined')})
 
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active')}
-#          ),
-#     )    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active')}
+         ),
+    )    
 
-#     search_fields = ('email',)
-#     ordering = ('email',)
+    search_fields = ('email',)
+    ordering = ('email',)
 
-# admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 from .managers import CustomUserManager
 
 # use the get_user_model() method from django.contrib.auth to refer 
 
 class CustomUser(AbstractUser):
+    username = models.CharField(
+        _('username'), 
+        max_length=150, 
+        unique=True, 
+        blank=True, 
+        null=True, 
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'), 
+        validators=[UnicodeUsernameValidator()], error_messages={
+            'unique': _("A user with that username already exists")
+    })
+    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(_('email address'), unique=True)
