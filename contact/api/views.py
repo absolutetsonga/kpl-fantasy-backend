4cb4e2ad094e3c3aec389b7c2852ec7250b42acc
absolutetsonga <abstsonga@gmail.com>
2024-02-01 16:03:02 +0600

    feat: is_responded field added, if any of the previous messages has is_responded false return error

--- contact/api/views.py
+++ contact/api/views.py
@@ -1,9 +1,23 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from ..models import Contact
 from .serializers import ContactSerializer
+from utils.error_handler import error_handler
 
 class ContactViewSet(ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        user_email = request.data.get('email', None)
+
+        if user_email:
+            previous_messages = Contact.objects.filter(email=user_email)
+
+            if previous_messages.filter(is_responded=False).exists():
+                return error_handler.bad_request_error({'detail': 'You must wait for a response to your previous messages before sending a new one.'})
+
+        return super().create(request, *args, **kwargs)
--- contact/models.py
+++ contact/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Contact(models.Model):
     email = models.EmailField()
     message = models.CharField(max_length=500)
+    is_responded = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Contact Message {self.id} from {self.email}"
