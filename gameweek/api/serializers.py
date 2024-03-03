098afe2736b155aed83e7c98b4991a433be3b8b5
absolutetsonga <abstsonga@gmail.com>
2024-03-03 11:06:52 +0500

    games serializer for gameweek

--- gameweek/api/serializers.py
+++ gameweek/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import GameWeek
 
+from games.api.serializers import GameSerializer
+
 class GameWeekSerializer(ModelSerializer):
+    games = GameSerializer(many=True, required=False)
+
     class Meta:
         model = GameWeek
         fields = '__all__'
